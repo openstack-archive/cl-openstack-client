770d6a71009df9a5c5e020818f7f2c589a899567
Julien Danjou <julien@danjou.info>
2013-06-25 17:03:55 +0200

    Add .gitreview, fix run-tests exit code
    Change-Id: I73e61f883ad549f9582d88df71acffd3e65d47e8

--- run-tests.lisp
+++ run-tests.lisp
@@ -1,4 +1,13 @@
+(defun terminate (status)
+  #+sbcl     (sb-ext:quit      :unix-status status)    ; SBCL
+  #+ccl      (   ccl:quit      status)                 ; Clozure CL
+  #+clisp    (   ext:quit      status)                 ; GNU CLISP
+  #+cmu      (  unix:unix-exit status)                 ; CMUCL
+  #+abcl     (   ext:quit      :status status)         ; Armed Bear CL
+  #+allegro  (  excl:exit      status :quiet t)        ; Allegro CL
+  (cl-user::quit))           ; Many implementations put QUIT in the sandbox CL-USER package.
+
 (require 'cl-openstack-client-test)
 (let ((results (5am:run 5am::*suite*)))
   (5am:explain! results)
-  (exit :code (if (eq (5am:results-status results ) t) 0 1)))
+  (terminate (if (eq (5am:results-status results ) t) 0 1)))
