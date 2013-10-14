4bff3d1699f54f46bd8ae31a7acdacc17ef48a07
Julien Danjou <julien@danjou.info>
2013-10-14 16:35:53 +0200

    Fix tests run
    Change-Id: I871160339a9de5c27aae8a128d5f57a535ac48d5
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+set -e
+set -x
 export HOME=$PWD/.test-env
 mkdir $HOME
 cd $HOME
 wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
-sbcl --load ../update-deps.lisp
+sbcl --script ../update-deps.lisp
+ls $HOME
+echo $HOME
+ln -s $PWD/.. ~/quicklisp/local-projects/cl-openstack-client
+sbcl --script ../run-tests.lisp
