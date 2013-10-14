c8cbca24db2d6bc4bbdb4663ad00c6d8a7cceaf0
Julien Danjou <julien@danjou.info>
2013-10-14 16:35:53 +0200

    Fix tests run
    Change-Id: I871160339a9de5c27aae8a128d5f57a535ac48d5
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,5 @@ export HOME=$PWD/.test-env
 mkdir $HOME
 cd $HOME
 wget -q http://beta.quicklisp.org/quicklisp.lisp -O quicklisp.lisp
-sbcl --load ../update-deps.lisp
+sbcl --script ../update-deps.lisp
+sbcl --script ../run-tests.lisp
